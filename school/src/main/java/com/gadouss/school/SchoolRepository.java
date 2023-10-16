b1dc12f0e4c0a6b35025b9c909279e33f383e023
[oussama-gadria] <gadria.oussama@esprit.tn>
2023-10-16 23:53:10 +0100

    update

--- school/src/main/java/com/gadouss/school/SchoolRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gadouss.school;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public  interface SchoolRepository extends JpaRepository<School, Integer> {
-}
--- student/src/main/java/com/gadouss/student/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gadouss.student;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public  interface StudentRepository extends JpaRepository<Student, Integer> {
-}
