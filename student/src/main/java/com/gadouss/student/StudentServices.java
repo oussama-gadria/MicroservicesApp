0cf115e6c7456380ce466ebcdf81bab1745fbc41
[oussama-gadria] <gadria.oussama@esprit.tn>
2023-10-17 16:12:49 +0100

    Feature : config of server config

---
+++ student/src/main/java/com/gadouss/student/StudentServices.java
@@ -0,0 +1,22 @@
+package com.gadouss.student;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudentServices {
+    private final StudentRepository repository;
+
+    public void saveStudent(Student student){
+        repository.save(student);
+    }
+
+    public List<Student> findAllStudents(){
+        return repository.findAll();
+    }
+
+    public List<Student> findAllStudentsBySchool(Integer schoolId){return repository.findAllBySchoolId(schoolId);}
+}
